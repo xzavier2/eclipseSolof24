e7309b41b8b6ae05058d676be4d1ba6e7bd0e2e3
xzavier2 <xbrooks1@emich.edu>
2024-09-25 14:46:19 -0400

    Just messed up lol

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -4,6 +4,7 @@ public class HelloBro {
 	public static void main(String [] args) {
 		System.out.println(" hi there !");
 		System.out.println("adding another statement");
+		System.out.println("why do I keep messing up");
 	}
 
 }
