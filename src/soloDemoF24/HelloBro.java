0fc7377a7b73a0df327b1eaaf7dbadabfef77551
xzavier2 <xbrooks1@emich.edu>
2024-09-25 14:34:31 -0400

    Added a statement EGIE

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -1,5 +1,9 @@
 package soloDemoF24;
 
 public class HelloBro {
+	public static void main(String [] args) {
+		System.out.println(" hi there !");
+		System.out.println("adding another statement");
+	}
 
 }
