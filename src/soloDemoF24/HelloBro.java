bd7737cbadd9ba664e643c09560f0140538e25fc
xzavier2 <xbrooks1@emich.edu>
2024-09-30 11:59:13 -0400

    omg

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -6,7 +6,7 @@ public class HelloBro {
 		System.out.println(" hi there !");
 		System.out.println("adding another statement");
 		System.out.println("why do I keep messing up");
-		System.out.println("fixing mistake");
+		System.out.println("fixing mistake lol");
 	}
 
 }
