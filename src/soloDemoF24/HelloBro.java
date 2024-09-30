8f423b6ec864aa1f3a8431243e34a57d17827758
xzavier2 <xbrooks1@emich.edu>
2024-09-30 12:19:11 -0400

    step 8.3

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -4,7 +4,7 @@ public class HelloBro {
 	
 	public static void main(String [] args) {
 		System.out.println(" hi there !");
-		System.out.println("adding another statement");
+		System.out.println("Progammer one was here");
 		System.out.println("why do I keep messing up");
 		System.out.println("fixing mistake lol");
 		System.out.println("Programmer 2 did this :)");
