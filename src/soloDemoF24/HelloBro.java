adad489f73b2373acf7321c99d91c80b3570b189
xzavier2 <xbrooks1@emich.edu>
2024-09-30 12:15:37 -0400

    progammer 1 did this 2 above

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -4,7 +4,7 @@ public class HelloBro {
 	
 	public static void main(String [] args) {
 		System.out.println(" hi there !");
-		System.out.println("adding another statement");
+		System.out.println("Programmer 1 did this");
 		System.out.println("why do I keep messing up");
 		System.out.println("fixing mistake lol");
 		System.out.println("Programmer 2 did this :)");
