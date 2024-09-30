ba711f64d27486d7dfb448c20f35ffb6c3a49302
xzavier2 <xbrooks1@emich.edu>
2024-09-30 11:55:56 -0400

    Fixing my mistake

--- src/soloDemoF24/HelloBro.java
+++ src/soloDemoF24/HelloBro.java
@@ -1,6 +1,7 @@
 package soloDemoF24;
 
 public class HelloBro {
+	
 	public static void main(String [] args) {
 		System.out.println(" hi there !");
 		System.out.println("adding another statement");
